68c9af279c3f18dc0f5c3c63220fb156f38ac65c
Antonio <Antonio@DESKTOP-J8ARSSP>
2020-07-09 09:41:46 +0200

    9-7-20 9:40 generación combinación primitiva

---
+++ 20_loteria_primitiva/src/principal/Loteria.java
@@ -0,0 +1,44 @@
+package principal;
+
+import java.util.Arrays;
+
+public class Loteria {
+
+	public static void main(String[] args) {
+		int[] combinacion=new int[6];
+		int total=0; //cuenta los números que se han generado hasta el momento
+		int generado;
+		while(total<6) {
+			generado=generarNumero();
+			if(!existente(combinacion,total,generado)) {
+				combinacion[total]=generado;
+				total++; //tenemos uno más
+			}
+		}
+		ordenarArray(combinacion);
+		mostrarCombinacion(combinacion);
+
+	}
+	
+	static int generarNumero() {
+		return (int)(Math.random()*49+1);
+	}
+
+	static boolean existente(int[] combinacion, int total,int generado) {
+		for(int i=0;i<total;i++) {
+			if(combinacion[i]==generado) {
+				return true;
+			}
+		}
+		return false; 
+	}
+	static void ordenarArray(int[] combinacion) {
+		Arrays.sort(combinacion);
+	}
+	static void mostrarCombinacion(int[] combinacion) {
+		for(int numero:combinacion) {
+			System.out.print(numero+", ");
+		}
+	}
+	
+}
